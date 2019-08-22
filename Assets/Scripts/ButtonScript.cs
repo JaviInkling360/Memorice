396a5bb4c504be02206fa428e346d3b45fbc8d85
F <F>
2019-08-22 12:07:28 -0400

    Añadidos archivos de los juegos

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform memoriceField;
+
+    [SerializeField]
+    private GameObject membtn;
+
+    void Awake()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject button = Instantiate(membtn);
+            button.name = "" + i;
+            button.transform.SetParent(memoriceField, false);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<Button> btns = new List<Button>();
+
+    void Awake()
+    {
+        
+    }
+
+
+
+}
