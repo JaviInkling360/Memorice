ca66bf3fba9cf9761e8348da00f0e0d44a157f79
F <F>
2019-09-26 13:28:47 -0300

    update 0

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,30 @@ public class GameController : MonoBehaviour
 {
     public List<Button> btns = new List<Button>();
 
-    void Awake()
+    [SerializeField]
+    private Sprite bgImage;
+
+    void Start()
     {
-        
+        GetButtons();
     }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Memorice Button");
+        
+        for(int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+
+    public void PickAPuzzle()
+    {
+        Debug.Log ("stop touching me you perv!");
+    }
+
 
 
 
