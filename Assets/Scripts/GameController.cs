968e9a88884a7c8fabc5e94bcef2b179f4f4d632
Javiera Letelier <javieralete@gmail.com>
2019-10-16 21:04:07 -0300

    update 4
    TAKE OVER IT'S TIME TO PUT YOU DOWN

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,40 +7,20 @@ public class GameController : MonoBehaviour
 {
     public List<Button> btns = new List<Button>();
 
-    [SerializeField]
-    private Sprite bgImage;
-
-    void Start()
+    void Awake()
     {
-        GetButtons();
-        AddListeners();
+        
     }
 
     void GetButtons()
     {
-        GameObject[] objects = GameObject.FindGameObjectsWithTag("Memorice Button");
-        
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Button");
+
         for(int i = 0; i < objects.Length; i++)
         {
             btns.Add(objects[i].GetComponent<Button>());
-            btns[i].image.sprite = bgImage;
         }
     }
-
-    void AddListeners()
-    {
-        foreach (Button btn in btns)
-        {
-            btn.onClick.AddListener(() => PickAPuzzle());
-        }
-    }
-
-    public void PickAPuzzle()
-    {
-        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-        Debug.Log ("hi i'm " + name);
-    }
-
 
 
 
